09c9cef2fb6c1781f104384063826f85f50d8535
LeonardoBayona <leo@branham.org>
2023-11-19 19:23:38 -0500

    Composed scenarios

--- CodingExercise/Exercise.cs
+++ CodingExercise/Exercise.cs
@@ -1,28 +1,19 @@
+using System.Runtime.CompilerServices;
+using Castle.Core.Internal;
+
+[assembly: InternalsVisibleTo(InternalsVisible.ToDynamicProxyGenAssembly2)]
 
 #region Tests
 
 namespace CodingExerciseTests
 {
     using Xunit;
+    using Moq;
     using CodingExerciseLogic;
 
     public class UnitTests
     {
 
-        [Theory]
-        [InlineData("", true)]
-        public void PhoneNumber_IsLiked_Test(string phone, bool expectedResponse)
-        {
-            // arrange
-            var sut = new PhoneNumbersUtils();
-        
-            // act
-            bool liked = sut.IsPhoneNumberLiked(phone);
-        
-            // assert
-            Assert.Equal(liked, expectedResponse);
-        }
-        
         #region HasSameDigitAtLeast3TimesContiguous
         
         [Theory]
@@ -67,7 +58,7 @@ namespace CodingExerciseTests
         public void Has_PhoneNumber_SameDigitAtLeast3TimesContiguous_Test(string phone, bool expectedResponse)
         {
             // arrange
-            var sut = new PhoneNumbersUtils();
+            var sut = new PhoneNumbersLikedUtils();
 
             // act
             bool liked = sut.HasSameDigitAtLeast3TimesContiguous(phone);
@@ -119,7 +110,7 @@ namespace CodingExerciseTests
         public void Has_PhoneNumber_SameDigitAtLeast4TimesAtAnyPosition_Test(string phone, bool expectedResponse)
         {
             // arrange
-            var sut = new PhoneNumbersUtils();
+            var sut = new PhoneNumbersLikedUtils();
 
             // act
             bool liked = sut.HasSameDigitAtLeast4TimesAtAnyPosition(phone);
@@ -130,15 +121,120 @@ namespace CodingExerciseTests
 
         #endregion
 
-        [Fact]
-        public void Test()
+        #region IsPhoneNumberLiked
+        
+        public static IEnumerable<object[]> IsLikedPhoneNumberData => 
+            new List<object[]>
+            {
+                // phone, hasSameDigitAtLeast4TimesAtAnyPosition, hasSameDigitAtLeast3TimesContiguous, expectedResponse
+                new object[] { "1234567890", false, false, false },
+                new object[] { "1112324256", false, true, true },
+                new object[] { "1123242526", true, false, true },
+                new object[] { "1112324252", true, true, true },
+            };
+        
+        [Theory, MemberData(nameof(IsLikedPhoneNumberData))]
+        public void PhoneNumber_IsLiked_E2ETest(
+            string phone, 
+            bool hasSameDigitAtLeast4TimesAtAnyPosition, 
+            bool hasSameDigitAtLeast3TimesContiguous, 
+            bool expectedResponse)
         {
             // arrange
-            
+            var mock = new PhoneNumbersLikedUtils();
+
             // act
-            
+            bool liked = mock.IsPhoneNumberLiked(phone);
+
             // assert
+            Assert.Equal(liked, expectedResponse);
         }
+
+        [Theory, MemberData(nameof(IsLikedPhoneNumberData))]
+        public void PhoneNumber_IsLiked_UnitTest(
+            string phone, 
+            bool hasSameDigitAtLeast4TimesAtAnyPosition, 
+            bool hasSameDigitAtLeast3TimesContiguous, 
+            bool expectedResponse)
+        {
+            // arrange
+            var sutMock = new Mock<PhoneNumbersLikedUtils>() { CallBase = true };
+            sutMock.Setup(x => x.HasSameDigitAtLeast4TimesAtAnyPosition(phone))
+                .Returns(hasSameDigitAtLeast4TimesAtAnyPosition);
+            sutMock.Setup(x => x.HasSameDigitAtLeast3TimesContiguous(phone))
+                .Returns(hasSameDigitAtLeast3TimesContiguous);
+
+            // act
+            bool liked = sutMock.Object.IsPhoneNumberLiked(phone);
+
+            // assert
+            Assert.Equal(liked, expectedResponse);
+        }
+        
+        #endregion
+        
+        #region ArePhoneNumbersLiked
+        
+        public static IEnumerable<object[]> AreLikedPhoneNumbersData => 
+            new List<object[]>
+            {
+                // phones, expectedResponse
+                
+                // 1
+                new object[] { new[] { "1234567890" }, 
+                    new[] { false } },
+                new object[] { new[] { "1112324252" }, 
+                    new[] { true } },
+                // 2
+                new object[] { new[] { "1234567890", "1112324252" }, 
+                    new[] { false, true } },
+                // 3
+                new object[] { new[] { "1234567890", "1123456788", "1112324256" }, 
+                    new[] { false, false, true } },
+                // 4
+                new object[] { new[] { "1234567890", "1123456788", "1112324256", "1123242526" }, 
+                    new[] { false, false, true, true } },
+                // 5
+                new object[] { new[] { "1234567890", "1123456788", "1122456788", "1112324256", "1123242526" }, 
+                    new[] { false, false, false, true, true } },
+            };
+        
+        [Theory, MemberData(nameof(AreLikedPhoneNumbersData))]
+        public void PhoneNumbers_AreLiked_E2ETest(
+            string[] phones, 
+            bool[] expectedResponse)
+        {
+            // arrange
+            var mock = new PhoneNumbersLikedUtils();
+
+            // act
+            bool[] likedArray = mock.ArePhoneNumbersLiked(phones);
+
+            // assert
+            Assert.Equal(likedArray, expectedResponse);
+        }
+        
+        [Theory, MemberData(nameof(AreLikedPhoneNumbersData))]
+        public void PhoneNumbers_AreLiked_UnitTest(
+            string[] phones, 
+            bool[] expectedResponse)
+        {
+            // arrange
+            var sutMock = new Mock<PhoneNumbersLikedUtils>() { CallBase = true };
+            for (int i = 0; i < phones.Length; i++)
+            {
+                sutMock.Setup(x => x.IsPhoneNumberLiked(phones[i]))
+                    .Returns(expectedResponse[i]);
+            }
+
+            // act
+            bool[] likedArray = sutMock.Object.ArePhoneNumbersLiked(phones);
+
+            // assert
+            Assert.Equal(likedArray, expectedResponse);
+        }
+        
+        #endregion
 
     }
 
@@ -150,21 +246,31 @@ namespace CodingExerciseTests
 
 namespace CodingExerciseLogic
 {
-    using Xunit;
 
-    public class PhoneNumbersUtils
+    public class PhoneNumbersLikedUtils
     {
         private const int MinNumTimesAtAnyPosition = 4;
         private const int MinNumTimesContiguous = 3;
-
-        public bool IsPhoneNumberLiked(string phone)
+        
+        #region ArePhoneNumbersLiked
+        
+        public bool[] ArePhoneNumbersLiked(string[] phones)
         {
-            return HasSameDigitAtLeast4TimesAtAnyPosition(phone) || HasSameDigitAtLeast3TimesContiguous(phone);
+            return phones.Select(IsPhoneNumberLiked)
+                .ToArray();
         }
+
+        public virtual bool IsPhoneNumberLiked(string phone)
+        {
+            return HasSameDigitAtLeast4TimesAtAnyPosition(phone) ||
+                   HasSameDigitAtLeast3TimesContiguous(phone);
+        }
+        
+        #endregion
         
         #region HasSameDigitAtLeast3TimesContiguous
 
-        internal bool HasSameDigitAtLeast3TimesContiguous(string phone)
+        public virtual bool HasSameDigitAtLeast3TimesContiguous(string phone)
         {
             const string digits = "0123456789";
             return digits.Any((char digit) => HasSameDigitAtLeast3TimesContiguous(phone, digit));
@@ -205,7 +311,7 @@ namespace CodingExerciseLogic
 
         #region HasSameDigitAtLeast4TimesAtAnyPosition
 
-        internal bool HasSameDigitAtLeast4TimesAtAnyPosition(string phone)
+        public virtual bool HasSameDigitAtLeast4TimesAtAnyPosition(string phone)
         {
             const string digits = "0123456789";
             return digits.Any((char digit) => HasSameDigitAtLeast4TimesAtAnyPosition(phone, digit));
@@ -231,6 +337,33 @@ namespace CodingExerciseLogic
         
     }
 
+    class Application { 
+  
+        // Main Method 
+        static public void Main_(string[] args)
+        {
+            string[] phoneNumbers = ReadPhoneNumbersFromConsole();
+            
+            PhoneNumbersLikedUtils utils = new();
+            bool[] likedFlags = utils.ArePhoneNumbersLiked(args);
+
+            for (int i = 0; i < likedFlags.Length; i++)
+            {
+                Console.WriteLine(likedFlags[i] ? "Y" : "N");
+            }
+        }
+
+        private static string[] ReadPhoneNumbersFromConsole()
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+            string[] phoneNumbers = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                phoneNumbers[i] = Console.ReadLine();
+            }
+            return phoneNumbers;
+        }
+    } 
 }
 
 #endregion
